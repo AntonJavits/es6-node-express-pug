bb858b7f010e5e9a3afe6119db25139147daf7c6
AntonJ <anton.javits@gmail.com>
2020-10-16 03:39:46 +0300

    clean-up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'pug')
 
-const fetch = require('node-fetch')
+//const fetch = require('node-fetch')
 const { combineData } = require('./businessLogic/combineData')
 
 app.get('/', async function (req, res) {
